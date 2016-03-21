737f2367491f3da112f826c2ff2ad0fc36756fcf
Leeheng Ma <Leeheng86@gmail.com>
2016-03-21 14:21:03 +0800

    initial commit for open stack machine wrapper.

---
+++ core/src/main/scala/sparkdeployer/OSMachines.scala
@@ -0,0 +1,122 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sparkdeployer
+
+import Helpers.retry
+//import com.amazonaws.regions.Regions
+//import com.amazonaws.services.ec2.AmazonEC2Client
+//import com.amazonaws.services.ec2.model.{BlockDeviceMapping, CreateTagsRequest, EbsBlockDevice, RunInstancesRequest, Tag, TerminateInstancesRequest}
+//import scala.util.{Failure, Success, Try}
+import org.slf4s.Logging
+import com.typesafe.config._
+import net.ceedubs.ficus.Ficus._
+
+import org.openstack4j.openstack.OSFactory
+import org.openstack4j.api._
+import org.openstack4j.model.compute._
+
+import scala.collection.JavaConverters._
+
+
+class OSMachines(config: Config) extends Machines with Logging {
+  class OSConf(config: Config) extends ClusterConf(config) {
+    val networkIds = config.as[Set[String]]("network-ids").toSeq
+    val imageId = config.as[String]("image-id")
+
+    val os_cacert = config.as[String]("os-cacert")
+    val os_auth_url = config.as[String]("os-auth-url")
+    val os_tenant_id = config.as[String]("os-tenant-id")
+    val os_tenant_name = config.as[String]("os-tenant-name")
+    val os_username = config.as[String]("os-username")
+    private val standardIn = System.console()
+    print("Password> ")
+    val os_password = standardIn.readPassword().toString
+    // TODO: attach extra disk
+    //val rootDevice = config.as[Option[String]]("root-device").getOrElse("/dev/xvda")
+  }
+  implicit val clusterConf = new OSConf(config)
+
+  private val os: OSClient = OSFactory.builder()
+    .endpoint(clusterConf.os_auth_url)
+    .credentials(clusterConf.os_username, clusterConf.os_password)
+    .tenantId(clusterConf.os_tenant_id)
+    .tenantName(clusterConf.os_tenant_name)
+    .useNonStrictSSLClient(true)
+    .authenticate()
+
+  def createMachines(machineType: MachineType, names: Set[String]): Seq[Machine] = {
+    names.toSeq.par.map { name =>
+      val flavor = os.compute().flavors().list().asScala.find(_.getName() == (machineType match {
+        case Master => clusterConf.masterInstanceType
+        case Worker => clusterConf.workerInstanceType
+      })).get
+      val image = os.compute().images().get(clusterConf.imageId)
+
+      val bs = Builders.server()
+      val serverCreate: ServerCreate = clusterConf.securityGroupIds
+                              .map(ids => ids.toSeq.foldLeft(bs) {(bs, id) => bs.addSecurityGroup(id)}).get
+                              .name(name)
+                              .flavor(flavor)
+                              .image(image)
+                              .keypairName(clusterConf.keypair)
+                              .networks(clusterConf.networkIds.toList.asJava)
+                              .build();
+      val server: Server = os.compute().servers().boot(serverCreate)
+      retry { i =>
+        val serverNow = os.compute().servers().list().asScala.find(_.getId() == server.getId()).get
+        assert(serverNow.getId() != null)
+        assert(serverNow.getName() != null)
+        val networkName = os.networking().network().get(clusterConf.networkIds.head).getName()
+        val addr = serverNow.getAddresses().getAddresses(networkName).asScala.head.getAddr()
+        assert(addr != null)
+        Machine(serverNow.getId(), serverNow.getName(), addr)
+      }
+    }.seq
+  }
+
+  private def getNonTerminatedInstances() = {
+    ???
+  //  ec2.describeInstances().getReservations.asScala.flatMap(_.getInstances.asScala).toSeq
+  //    .filter(_.getKeyName == clusterConf.keypair)
+  //    .filter(_.getState.getName != "terminated")
+  }
+
+  def destroyMachines(ids: Set[String]) = {
+    ???
+  //  val existingIds = getNonTerminatedInstances.map(_.getInstanceId).toSet & ids
+
+  //  if (existingIds.nonEmpty) {
+  //    log.info(s"[EC2] Terminating ${existingIds.size} instances.")
+  //    ec2.terminateInstances(new TerminateInstancesRequest(existingIds.toSeq.asJava))
+  //    retry { i =>
+  //      log.info(s"[EC2] Checking status. Attempts: $i.")
+  //      val nonTerminatedTargetInstances = getNonTerminatedInstances.map(_.getInstanceId).toSet & existingIds
+  //      assert(nonTerminatedTargetInstances.isEmpty, "[EC2] Some instances are not terminated: " + nonTerminatedTargetInstances.mkString(","))
+  //    }
+  //    log.info("[EC2] All instances are terminated.")
+  //  }
+  }
+
+  def getMachines() = {
+    ???
+  //  getNonTerminatedInstances.map { i =>
+  //    Machine(
+  //      i.getInstanceId,
+  //      i.getTags().asScala.find(_.getKey == "Name").map(_.getValue).getOrElse(""),
+  //      if (clusterConf.usePrivateIp) i.getPrivateIpAddress else i.getPublicDnsName
+  //    )
+  //  }
+  }
+}
